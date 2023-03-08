1afe90f20c8257b6b2fc3fc695100cc65d867083
KYOJINNAAPYON <kyojinnaapyon@yahoo.co.jp>
2023-03-09 00:12:19 +0900

    フッターを部品化

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -19,10 +19,12 @@
 <body style="padding: 60px 0;">
     <div id="app">
         @include('layouts.header')    
-        
+
         <main class="py-4">
             @yield('content')
         </main>
+
+        @include('layouts.footer')
     </div>
 </body>
 </html>
